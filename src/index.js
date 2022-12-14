a4a26181ee9911f33fdf01615b874ee39ec01a2d
Alex Schneider <me@schneider.ax>
2022-12-14 21:54:52 +0600

    Remove server URL from the initial log message

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import scope from './scope.js'
 let { config, server } = scope()()
 
 server.start(() => {
-  console.log(`LDAP server listening at ${server.url}`)
+  console.log('LDAP server listening')
   console.log()
   console.log(`Configuration:\n${JSON.stringify(config, null, 4)}`)
 })
